b0e4005ae320a650369995aa3e95061ccdf68cd9
Patrick Charles <patrickrcharles@gmail.com>
2020-02-05 15:03:02 -0600

    refactoring of main app
    moving some code to function to streamline main[ ]

--- src/main/java/com/springboot/titletech/StageTestData.java
+++ src/main/java/com/springboot/titletech/StageTestData.java
@@ -17,50 +17,131 @@ import java.util.Random;
 
 public class StageTestData {
 
+    // database connect info
+    protected static String url = "jdbc:mysql://localhost/title-ownership?useSSL=false";
+    protected static String user = "root";
+    protected static String password = "admin";
+
+    protected static String insertPersonDataQuery =
+            "INSERT INTO person (first_name, middle_name, last_name, parcelid) values (?, ?, ?, ?)";
+    protected static String insertParcelDataQuery = "INSERT INTO parcel (street, city, state, zip_code) values (?, ?, ?,?)";
+
     public static void main(String[] args) throws URISyntaxException {
 
-        // get json file paths from resource folder
+        // get json file paths from resource folder. this is good enough for now
+        // todo: write a class to load properties file
+
+        //get paths to json test data files
         ClassLoader classLoader = StageTestData.class.getClassLoader();
         String jsonNamesPath = classLoader.getResource("names.json").getPath();
         String jsonAddressesPath = classLoader.getResource("addresses.json").getPath();
         String jsonBoughtSoldTimestampsPath = classLoader.getResource("timestamps.json").getPath();
 
-        JSONParser parser = new JSONParser();
+        // get jsonArrays of test data for parsing/insertion to database
+        JSONArray personList = getJsonArrayFromFile(jsonNamesPath);
+        JSONArray parcelList = getJsonArrayFromFile(jsonAddressesPath);
 
+        try (Reader reader = new FileReader(jsonAddressesPath)) {
+            // insert person data into array
+            // personIndex is for keeping track of the index of the person object in the jsonArray
+            final int[] personIndex = new int[2000];
+            personList.forEach(person -> {
+                InsertPersonObjectToDB((JSONObject) person, personIndex[0]);
+                // increment for next person.
+                personIndex[0]++;
+            });
+            // insert Parcel data into database
+            personList.forEach(person -> {
+                InsertParcelObjectToDB((JSONObject) person);
+            });
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static JSONArray getJsonArrayFromFile(String jsonNamesPath) {
+
+        JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader(jsonNamesPath)) {
 
             Object obj = parser.parse(reader);
-            JSONArray personList = (JSONArray) obj;
-            //System.out.println(personList);
+            JSONArray jsonArray = (JSONArray) obj;
 
-            // begin index tracker. used to access array of shuffled parcel ids
-            // called in InsertPersonObjectToDB() function.
-            // function is used to quasi randomize data
-            final int[] personIndex = new int[2000];
-            // loop through each name and insert into DB while incrementing
-            // the tracking the current index of the object in jsonArray
-            personList.forEach(person -> {
-                InsertPersonObjectToDB((JSONObject) person, personIndex[0]);
-                personIndex[0]++;
-            });
+            return jsonArray;
 
         } catch (IOException | ParseException exception) {
             exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void InsertPersonObjectToDB(JSONObject person, int index) {
+
+        String firstName = (String) person.get("first_name");
+        String lastName = (String) person.get("last_name");
+        String middleName = (String) person.get("middle_name");
+
+        int[] parcelid = getShuffledIntegerArray(2000);
+
+        if (index >= 2000) {
+            index = index - 2000;
+            //another hack. 5000 persons, 2000 addresses.
+            // reset index manually so we can have duplicate addresses
         }
 
-        try (Reader reader = new FileReader(jsonAddressesPath)) {
+        // todo: this is a hack. for some reason data inserts correctly but index still hits 2000 and throws
+        // out of bounds error. figure it out later
 
-            Object obj = parser.parse(reader);
-            JSONArray personList = (JSONArray) obj;
-            //System.out.println(personList);
-            //Iterate over array
+        if (index < 2000) {
+            try {
 
-            personList.forEach(person -> {
-                InsertAddressObjectToDB((JSONObject) person);
-            });
+                Connection conn = DriverManager.getConnection(url, user, password);
 
-        } catch (IOException | ParseException exception) {
-            exception.printStackTrace();
+                PreparedStatement statement = conn.prepareStatement(insertPersonDataQuery);
+                statement.setString(1, firstName);
+                statement.setString(2, middleName);
+                statement.setString(3, lastName);
+                statement.setInt(4, parcelid[index]);
+
+                int row = statement.executeUpdate();
+                if (row > 0) {
+                    System.out.println("Person data entered successfully");
+                }
+                statement.close();
+                conn.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private static void InsertParcelObjectToDB(JSONObject address) {
+        //Get employee object within list
+        //JSONObject employeeObject = (JSONObject) employee.get("first_name");
+
+        String city = (String) address.get("city");
+        String street = (String) address.get("street");
+        String state = (String) address.get("state");
+        String zipCode = (String) address.get("zip_code");
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            PreparedStatement statement = conn.prepareStatement(insertParcelDataQuery);
+            statement.setString(1, street);
+            statement.setString(2, city);
+            statement.setString(3, state);
+            statement.setString(4, zipCode);
+
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                System.out.println("address data entered successfully");
+            }
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -84,97 +165,5 @@ public class StageTestData {
         }
         // System.out.println("parcelids size : "+parcelids.length);
         return parcelids;
-
     }
-
-    private static void InsertPersonObjectToDB(JSONObject person, int index) {
-        //System.out.println("person : " + person);
-        String firstName = (String) person.get("first_name");
-        String lastName = (String) person.get("last_name");
-        String middleName = (String) person.get("middle_name");
-
-        int[] parcelid = getShuffledIntegerArray(2000);
-
-        if (index >= 2000) {
-            index = index - 2000;
-            //another hack. 5000 persons, 2000 addresses.
-            // reset index manually so we can have duplicate addresses
-        }
-
-        // this is a hack. for some reason data inserts correctly but index still hits 2000 and throws
-        // out of bounds error
-
-//            System.out.println(firstName + " " + middleName + " " + lastName);
-//            System.out.println("personIndex = " + index);
-//            System.out.println("parcelid[" + index + "] = " + parcelid[index]);
-//
-//            System.out.println("parcelid[" + index + "] = " + parcelid[index]);
-//            System.out.println("parcelid.size = " + parcelid.length);
-
-
-        String url = "jdbc:mysql://localhost/title-ownership?useSSL=false";
-        String user = "root";
-        String password = "admin";
-
-        if (index < 2000) {
-            try {
-
-                Connection conn = DriverManager.getConnection(url, user, password);
-                String sqlQuery = "INSERT INTO person (first_name, middle_name, last_name, parcelid) values (?, ?, ?, ?)";
-
-                PreparedStatement statement = conn.prepareStatement(sqlQuery);
-                statement.setString(1, firstName);
-                statement.setString(2, middleName);
-                statement.setString(3, lastName);
-                statement.setInt(4, parcelid[index]);
-
-                int row = statement.executeUpdate();
-                if (row > 0) {
-                    System.out.println("Person data entered successfully");
-                }
-                statement.close();
-                conn.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    private static void InsertAddressObjectToDB(JSONObject address) {
-        //Get employee object within list
-        //JSONObject employeeObject = (JSONObject) employee.get("first_name");
-
-        String city = (String) address.get("city");
-        String street = (String) address.get("street");
-        String state = (String) address.get("state");
-        String zipCode = (String) address.get("zip_code");
-
-        //System.out.println(street + "\n" + city + " , " + state + " " + zipCode+"\n");
-
-        String url = "jdbc:mysql://localhost/title-ownership?useSSL=false";
-        String user = "root";
-        String password = "admin";
-
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            String sql = "INSERT INTO parcel (street, city, state, zip_code) values (?, ?, ?,?)";
-
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setString(1, street);
-            statement.setString(2, city);
-            statement.setString(3, state);
-            statement.setString(4, zipCode);
-
-            int row = statement.executeUpdate();
-            if (row > 0) {
-                System.out.println("address data entered successfully");
-            }
-            statement.close();
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
 }
